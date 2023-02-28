8cbe32e98d96032ff1f973777003f5696b31ef69
Sakshi785 <123529467+Sakshi785@users.noreply.github.com>
2023-02-28 20:17:59 +0530

    Update sanya.c

--- sanya.c
+++ sanya.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 int main()
 {
@@ -8,5 +9,17 @@ int main()
   scanf("%d", &b);
   
   printf("The sum of two nmbers is %d", a+b);
+  return 0;
+}
+*/
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter first number:");
+  scanf("%d",&a);
+  printf("Enter second number:");
+  scanf("%d",&b);
+  printf("Subtraction of two numbers are : %d",a-b);
   return 0;
 }
